6128af78cbeda863051587fbc5c0ec41c981d1f8
Mahmut OZYURT <mtoz147@gmail.com>
2023-09-24 20:02:48 +0300

    -The Pixabay website's API has been defined for image search. -Util package has been created for defining constants. -Api package has been created. -ImageResponse data class has been created under the model package. -ImageResult data class has been created under the model package. -Retrofit API has been created under api package.  -Retrofit components have been implemented. -Internet permission has been added to manifest.xml

---
+++ app/src/main/java/com/mtoz147/artbooktestinghilt/model/ImageResponse.kt
@@ -0,0 +1,9 @@
+package com.mtoz147.artbooktestinghilt.model
+
+data class ImageResponse(
+    val hits:List<ImageResult>,
+    val total:Int,
+    val totalHits:Int
+
+
+)
---
+++ app/src/main/java/com/mtoz147/artbooktestinghilt/util/Util.kt
@@ -0,0 +1,6 @@
+package com.mtoz147.artbooktestinghilt.util
+
+object Util {
+    const val API_KEY = "39642108-f70e1b17d61357fbb83d12364"
+    const val BASE_URL= "https://pixabay.com"
+}
